de957edb898a41abd5d67711a4867fa8ff606b6d
MMD <manuelmoralesdiaz0@gmail.com>
2024-04-19 17:56:47 -0400

    updating users

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -7,9 +7,11 @@ from models.user import User
 
 
 @app_views.route("/users", methods=["GET"], strict_slashes=False)
-def get_users():
+def get_users(user_id):
     """Retrieves the list of all user objects"""
-    users = storage.all("User").values()
+    users = storage.get(User, user_id)
+    if not users:
+        abort(404)
     users_list = []
     for user in users:
         users_list.append(user.to_dict())
@@ -19,7 +21,7 @@ def get_users():
 @app_views.route("/users/<user_id>", methods=["GET"], strict_slashes=False)
 def get_user(user_id):
     """Retrieves a user object based on its id"""
-    user = storage.get("User", user_id)
+    user = storage.get(User, user_id)
     if not user:
         abort(404)
     return jsonify(user.to_dict())
@@ -34,12 +36,13 @@ def delete_user(user_id):
     else:
         storage.delete(user)
         storage.save()
-        return jsonify({}), 200
+        return jsonify({}, 200)
 
 
 @app_views.route("/users", methods=["POST"], strict_slashes=False)
 def post_user():
     """Creates a new user object"""
+    
     if not request.is_json:
         abort(400, "Not a JSON")
     if "email" not in request.get_json():
@@ -50,3 +53,19 @@ def post_user():
     new_user = User(**data)
     new_user.save()
     return jsonify(new_user.to_dict()), 201
+
+
+@app_views.route("/users/<user_id>", methods=["PUT"], strict_slashes=False)
+def put_user(user_id):
+    """Updates a user object based on its id"""
+    user = storage.get(User, user_id)
+    if not user:
+        abort(404)
+    if not request.is_json:
+        abort(400, "Not a JSON")
+    data = request.get_json()
+    for key, value in data.items():
+        if key not in ["id", "email", "created_at", "updated_at"]:
+            setattr(user, key, value)
+    user.save()
+    return jsonify(user.to_dict()), 200
