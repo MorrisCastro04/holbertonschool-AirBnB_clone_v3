694da9d51fa434ac217658038166a37ca8a58688
MMD <manuelmoralesdiaz0@gmail.com>
2024-04-19 19:23:16 -0400

    reverting all changes

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -5,7 +5,7 @@ from models import storage
 from api.v1.views import app_views
 from models.place import Place
 from models.city import City
-from models.user import User
+from models.user import user_id
 
 
 @app_views.route("/cities/<city_id>/places", methods=["GET"],
@@ -51,21 +51,17 @@ def post_place(city_id):
     city = storage.get(City, city_id)
     if not city:
         abort(404)
-    data = request.get_json()
-    if data is None:
+    if not request.is_json:
         abort(400, "Not a JSON")
-    if 'user_id' not in data.keys():
+    if "user_id" not in request.get_json():
         abort(400, "Missing user_id")
-    user = storage.get(User, data['user_id'])
-    if user is None:
-        abort(404)
-    if 'name' not in data.keys():
+    if "name" not in request.get_json():
         abort(400, "Missing name")
-    place = Place(**data)
-    place.city_id = city_id
-    place.save()
-    place = place.to_json()
-    return jsonify(place), 201
+    data = request.get_json()
+    data['city_id'] = city_id
+    new_place = Place(**data)
+    new_place.save()
+    return jsonify(new_place.to_dict()), 201
 
 
 @app_views.route("/places/<place_id>", methods=["PUT"], strict_slashes=False)
